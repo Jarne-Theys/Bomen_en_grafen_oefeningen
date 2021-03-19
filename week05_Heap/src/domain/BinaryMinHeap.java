820c84275e7bdea2c16e62cbfde01e470d8b7299
Jarne-Theys <jarne.theys@student.ucll.be>
2021-03-19 09:35:44 +0100

    Week 5 compleet

--- week05_Heap/src/domain/BinaryMinHeap.java
+++ week05_Heap/src/domain/BinaryMinHeap.java
@@ -20,7 +20,7 @@ public class BinaryMinHeap<E extends Comparable<E>> {
         if (this.isEmpty())
             throw new IllegalStateException("Kan niet zoeken in een lege heap");
         //TO DO zie oefening 3
-        return null;
+        return values.get(0);
     }
 
     public boolean addValue(E value) {
@@ -36,7 +36,23 @@ public class BinaryMinHeap<E extends Comparable<E>> {
     }
 
     private void bubbleUp() {
-        //TO DO : oefening 4
+        int index = (values.size() - 1);
+        int indexOuder = (index - 1) / 2;
+        while (indexOuder >= 0) {
+            if (values.get(index).compareTo(values.get(indexOuder)) < 0) {
+                switchNodes(index, indexOuder);
+                index = indexOuder;
+                indexOuder = (indexOuder - 1) / 2;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void switchNodes(int index, int indexOuder) {
+        E hulp = values.get(index);
+        values.set(index, values.get(indexOuder));
+        values.set(indexOuder, hulp);
     }
 
     public E removeSmallest() {
@@ -49,12 +65,74 @@ public class BinaryMinHeap<E extends Comparable<E>> {
         return res;
     }
 
-    private void bubbleDown() {
-        // TODO zie oefening 5
+    /*private void bubbleDown() {
+        int index = 0;
+        int indexLinkerKind = 1;
+        int indexRechterKind = 2;
+        while () {
+            if (values.get(index).compareTo(values.get(indexLinkerKind)) > values.get(0).compareTo(values.get(indexRechterKind))) {
+                switchNodes(indexRechterKind, index);
+                index = indexRechterKind;
+                indexRechterKind = indexRechterKind * 2 + 2;
+            } else {
+                switchNodes(indexLinkerKind, index);
+                index = indexLinkerKind;
+                indexLinkerKind = indexLinkerKind * 2 + 1;
+            }
+        }
+    }*/
+
+    private int indexRechterKind(int index) {
+        return index * 2 + 2;
     }
 
-    public ArrayList<E> getPath(E value) {
-        // TODO zie oefening 6;
-        return null;
+    private int indexLinkerKind(int index) {
+        return index * 2 + 1;
     }
+
+    private boolean kindBestaat(int index){
+        if(index >= values.size()){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void bubbleDown() {
+        int index = 0;
+        boolean wisselOK = true;
+
+        while (kindBestaat(indexLinkerKind(index))&& wisselOK) {
+
+            int indexKleinsteKind = indexLinkerKind(index);
+
+            if (kindBestaat(indexRechterKind(index))
+                    && values.get(indexKleinsteKind).compareTo(values.get(indexRechterKind(index))) > 0) {
+                indexKleinsteKind = indexRechterKind(index);
+            }
+
+            if (values.get(index).compareTo(values.get(indexKleinsteKind)) > 0) {
+
+                this.switchNodes(index, indexKleinsteKind);
+            } else {
+                wisselOK = false;
+            }
+            index = indexKleinsteKind;
+        }
+    }
+
+    public ArrayList<E> getPath (E value){
+        ArrayList<E> path = new ArrayList<>();
+        int index = values.indexOf(value);
+        if(index == -1){
+            return null;
+        }
+        path.add(value);
+        while(index > 0){
+            index = (index - 1) / 2;
+            path.add(0,values.get(index));
+        }
+        return path;
+    }
+
 }
--- week05_Heap/src/ui/BinaryMinHeapDriver.java
+++ week05_Heap/src/ui/BinaryMinHeapDriver.java
@@ -16,18 +16,22 @@ public class BinaryMinHeapDriver {
 		heap.addValue(2);
 		heap.addValue(-7);
 		heap.print();
-		System.out.println();
 		System.out.println("Kleinste waarde = " + heap.getMin());
-		System.out.println();
-		
+		System.out.println("\n5 x Kleinste waarde verwijderen:");
+
+
 		for (int i = 1 ; i <= 5; i++){
 			System.out.println(heap.removeSmallest());
 			heap.print();
 		}
-		
+
+		System.out.println("\nGet path van 3, 2 en 5:");
+
 		System.out.println(heap.getPath(3));
 		System.out.println(heap.getPath(2));
 		System.out.println(heap.getPath(5));
+
+
 	}
 
 }
